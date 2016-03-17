e0ebeaf45f24e1b7f8e86467aa98d3e69e411838
PeschanskyVlad <kpi.kp52.peschansky@gmail.com>
2016-03-17 21:04:10 +0200

    c

--- interface/widget.cpp
+++ interface/widget.cpp
@@ -1,16 +1,49 @@
 #include "widget.h"
+#include "time.h"
 #include "ui_widget.h"
+#include <Windows.h>
+
+#include <Qt>
+
+void Widget::keyPressEvent(QKeyEvent * event){
+    switch(event->key()){
+    case Qt::Key_W:
+       // ui->pushButton->setStyleSheet("QPushButton{background-color: lightgrey; border-style: outset; border-width: 5px; border-color: red; }");
+       // Sleep(10);
+       // ui->pushButton->setStyleSheet("QPushButton{background-color: lightgrey; border-style: outset; border-width: 5px; border-color: gray; }");
+        carAcceleration();
+        break;
+    case Qt::Key_A:
+        ui->textEdit->setText("a");
+        break;
+    case Qt::Key_S:
+        ui->textEdit->setText("s");
+        carBraking();
+        break;
+    case Qt::Key_D:
+        ui->textEdit->setText("d");
+        break;
+    }
+
+    //ui->textEdit->setText(event->text());
+
+}
 
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Widget)
 {
     carState=false;
+    programCarSpeed=50;
     electromagnetState=false;
     ui->setupUi(this);
     QObject::connect(ui->pushButton_5,SIGNAL(clicked()),this,SLOT(carStatus()));
     QObject::connect(ui->pushButton_6,SIGNAL(clicked()),this,SLOT(electromagnetStatus()));
     QObject::connect(ui->pushButton_7, SIGNAL(clicked()), this, SLOT(exit()));
+    QObject::connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(carAcceleration()));
+    QObject::connect(ui->pushButton_2, SIGNAL(clicked()), this, SLOT(carBraking()));
+    QObject::connect(ui->verticalSlider, SIGNAL(valueChanged(int)), this, SLOT(cangeCarPrograSpeed()));
+
     ui->pushButton->setStyleSheet("QPushButton{background-color: lightgrey; border-style: outset; border-width: 5px; border-color: gray; }"
 "QPushButton:hover{background-color: lightgrey; border-style: outset; border-width: 5px; border-color: red;}");
 
@@ -30,7 +63,7 @@ Widget::Widget(QWidget *parent) :
     ui->pushButton_7->setStyleSheet("QPushButton{background-color: lightgrey; border-style: outset; border-width: 5px; border-color: gray; }"
 "QPushButton:hover{background-color: lightgrey; border-style: outset; border-width: 5px; border-color: red;}");
 
-    ui->verticalSlider->setValue(50);
+    ui->verticalSlider->setValue(programCarSpeed);
 
 
 }
@@ -42,15 +75,20 @@ Widget::~Widget()
 
 void Widget::carStatus()
 {
+    carState = !carState;
     if(carState){
+        programCarSpeed=50;
+        ui->verticalSlider->setValue(programCarSpeed);
      ui->textEdit->setText("Online");
 
     }
     else {
+        programCarSpeed=50;
+        ui->verticalSlider->setValue(programCarSpeed);
         ui->textEdit->setText("Offline");
 
     }
-    carState = !carState;
+
 }
 
 void Widget::electromagnetStatus()
@@ -70,6 +108,28 @@ void Widget::exit()
 {
     QApplication::exit();
 
+}
+
+void Widget::carAcceleration()
+{
+    if (programCarSpeed<100){
+        programCarSpeed=programCarSpeed+10;
+    }
+    ui->verticalSlider->setValue(programCarSpeed);
+   // ui->label_10->setText(QString("%1").arg(programCarSpeed));
+}
+
+void Widget::carBraking()
+{
+    if (programCarSpeed>0){
+        programCarSpeed=programCarSpeed-10;
+    }
+    ui->verticalSlider->setValue(programCarSpeed);
+}
+
+void Widget::cangeCarPrograSpeed()
+{
+    programCarSpeed = ui->verticalSlider->value();
 }
 
 
--- interface/widget.h
+++ interface/widget.h
@@ -2,6 +2,8 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QtGui>
+#include <string>
 
 namespace Ui {
     class Widget;
@@ -16,15 +18,26 @@ public:
     ~Widget();
 
 private:
+    virtual void keyPressEvent(QKeyEvent * event);
+    //virtual void sliderMoved();
     Ui::Widget *ui;
     bool carState;
     bool electromagnetState;
+    int programCarSpeed;
+
 public slots:
     void carStatus();
 public slots:
     void electromagnetStatus();
 public slots:
     void exit();
+public slots:
+    void carAcceleration();
+public slots:
+    void carBraking();
+public slots:
+    void cangeCarPrograSpeed();
 };
+
 
 #endif // WIDGET_H
