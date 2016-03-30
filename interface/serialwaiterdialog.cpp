06067dc848259b3b30f185bc61ce9d9ecfd6eb3b
PeschanskyVlad <kpi.kp52.peschansky@gmail.com>
2016-03-30 09:46:50 +0300

    New form

---
+++ interface/serialwaiterdialog.cpp
@@ -0,0 +1,45 @@
+#include "serialwaiterdialog.h"
+#include <QtSerialPort/QSerialPortInfo>
+#include <QLabel>
+#include "ui_serialreaderdialog.h"//TODO remove
+
+QString SerialWaiterDialog::get_selection(){
+    if(selected)
+        return this->user_selection;
+    else return QString::null;
+}
+
+void SerialWaiterDialog::selectPort(int index){
+    this->user_selection = form->port_list->currentData().toString();
+    selected = true;
+    form->confirm_button->setEnabled(true);
+}
+
+void SerialWaiterDialog::refreshPortList(){
+    selected = false;
+    form->confirm_button->setEnabled(false);
+
+    form->port_list->clear();
+    for(QSerialPortInfo& info : QSerialPortInfo::availablePorts()){
+        form->port_list->addItem(info.portName()+" "+info.description(),info.portName());
+    }
+
+    if(form->port_list->count() == 1){
+        form->port_list->setCurrentIndex(0);
+        selectPort(0);
+    }
+}
+
+
+SerialWaiterDialog::SerialWaiterDialog(QWidget* parent)
+    : QDialog(parent, Qt::WindowTitleHint | Qt::WindowCloseButtonHint),
+      form(new Ui::Dialog)
+{
+    form->setupUi(this);
+    connect(form->refresh_button,&QPushButton::clicked,this, &SerialWaiterDialog::refreshPortList);
+    connect(form->port_list,static_cast<void (QComboBox::*)(int n)>(&QComboBox::activated),
+            this,&SerialWaiterDialog::selectPort);
+    connect(form->confirm_button,&QPushButton::clicked,this,[=](){this->accept();});
+    refreshPortList();
+    selected = false;
+}
